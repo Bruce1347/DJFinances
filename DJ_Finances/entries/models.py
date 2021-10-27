c4c13ee34ab3ef361094341fdc5f21c53e58fa71
Kaci Adjou <adjou.kaci@gmail.com>
2021-10-27 22:54:16 +0200

    [models] Add an Entry model
    An Entry is a timestamped model that represents a transaction with the
    following attributes:
    	* amount: a Decimal field that can be positive (income) or negative (expense)
    	* title: a short Char field that sums up the transaction topic
    	* comment: a longer Char field if a longer description is needed for
    	that specific transaction.

--- DJ_Finances/entries/models.py
+++ DJ_Finances/entries/models.py
@@ -1,3 +1,45 @@
+import uuid
+
+from django.core.exceptions import ObjectDoesNotExist
 from django.db import models
 
-# Create your models here.
+
+class Entry(models.Model):
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False,
+    )
+    amount = models.DecimalField(
+        null=False,
+        max_digits=7,
+        decimal_places=2
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+    )
+    title = models.CharField(
+        max_length=300,
+    )
+    comment = models.TextField(
+        blank=True,
+        null=False,
+        default="",
+    )
+
+    def to_dict(self):
+        return {
+            "id": str(self.id),
+            "title": self.title,
+            "comment": self.comment,
+            "amount": str(self.amount),
+            "created_at": self.created_at.strftime("%s"),
+        }
+
+    @classmethod
+    def get_entry_by_id(cls, entry_id):
+        try:
+            entry = cls.objects.get(id=entry_id)
+            return entry
+        except ObjectDoesNotExist:
+            return None
